26abccdbbee0771213dabfac1fcac6a78dd68753
Francesco Iapicca <iapicca@gmail.com>
2020-06-04 20:51:55 +0300

    format yak_runner stream generator

--- yak_runner/lib/src/streams/yak_stream_generator.dart
+++ yak_runner/lib/src/streams/yak_stream_generator.dart
@@ -4,24 +4,16 @@ import 'package:meta/meta.dart';
 import 'package:yak_runner/yak_runner.dart';
 
 class YakStreamGenerator<T> {
- final FutureOr<Either<Failure,T>> Function() function;
+  final FutureOr<Either<Failure, T>> Function() function;
   final Function(Failure failure) onFailure;
-  const YakStreamGenerator(
-    {
-      @required this.function,
-      this.onFailure,
-    }) 
-    : assert(function != null);
+  const YakStreamGenerator({
+    @required this.function,
+    this.onFailure,
+  }) : assert(function != null);
 
-  Stream<T> call({
-    @required Stream inbound
-  }) => StreamTransformer
-    .fromHandlers(
-      handleData: (_, Sink<T> sink) async {
-      final Either<Failure,T> _processed = await function();
-      _processed.fold(
-        (l) => onFailure(l),
-        (r) => sink.add(r));
-    })
-    .bind(inbound);
+  Stream<T> call({@required Stream inbound}) =>
+      StreamTransformer.fromHandlers(handleData: (_, Sink<T> sink) async {
+        final Either<Failure, T> _processed = await function();
+        _processed.fold((l) => onFailure(l), (r) => sink.add(r));
+      }).bind(inbound);
 }
