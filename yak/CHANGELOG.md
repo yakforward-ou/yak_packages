7e5c8db5ecf342318c5e8501945d29c15b9167ef
Francesco Iapicca <iapicca@gmail.com>
2020-06-07 01:17:04 +0300

    stream generator + test DONE

--- yak/CHANGELOG.md
+++ yak/CHANGELOG.md
@@ -1,7 +1,16 @@
+## [0.0.1+1] - 4th June 2020
+
+* this release is pre-alpha
+
+| TO DO LIST | STATUS  |
+|--------|-----|
+| example| x |
+
+
 ## [0.0.1] - 4th June 2020
 
 * this release is pre-alpha
 
-| TO DO LIST |  |
+| TO DO LIST | STATUS |
 |--------|-----|
 | add [yak_runner] package| done |
--- yak_runner/lib/src/streams/yak_stream_generator.dart
+++ yak_runner/lib/src/streams/yak_stream_generator.dart
@@ -4,16 +4,21 @@ import 'package:meta/meta.dart';
 import 'package:yak_runner/yak_runner.dart';
 
 class YakStreamGenerator<T> {
-  final FutureOr<Either<Failure, T>> Function() function;
-  final Function(Failure failure) onFailure;
-  const YakStreamGenerator({
-    @required this.function,
-    this.onFailure,
-  }) : assert(function != null);
+  final YakAsyncGenerator generator;
+  final void Function(Failure failure) onFailure;
+  const YakStreamGenerator(
+    {
+    @required this.generator,
+    @required this.onFailure,
+  }): assert(generator != null);
 
-  Stream<T> call({@required Stream inbound}) =>
-      StreamTransformer.fromHandlers(handleData: (_, Sink<T> sink) async {
-        final Either<Failure, T> _processed = await function();
-        _processed.fold((l) => onFailure(l), (r) => sink.add(r));
-      }).bind(inbound);
+  Stream<T> call({@required Stream stream}) =>  StreamTransformer.fromHandlers(
+        handleData: (dynamic data, Sink<T> sink) async {
+          final Either<Failure, T> _processed = await generator();
+          _processed.fold(
+            (Failure l) => onFailure(l),
+            (T r) => sink.add(r),
+          );
+        },
+      ).bind(stream);
 }
