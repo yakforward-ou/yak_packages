ac91c680eed3c1e2c10b7d3855088e1afcef90a7
Francesco Iapicca <iapicca@gmail.com>
2021-01-27 17:01:44 +0200

    yak import/export yak_math

--- yak/lib/src/external/all.dart
+++ yak/lib/src/external/all.dart
@@ -2,3 +2,4 @@ export 'package:yak_runner/yak_runner.dart';
 export 'package:yak_test/yak_test.dart';
 export 'package:yak_error_handler/yak_error_handler.dart';
 export 'package:yak_widgets/yak_widgets.dart';
+export 'package:yak_math/yak_math.dart';
