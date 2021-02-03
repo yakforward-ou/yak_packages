b79b2531a7882465fbb87c9e802afdab11721fcf
Francesco Iapicca <iapicca@gmail.com>
2021-02-03 16:33:14 +0200

    yak_error_handler new example in pure dart

--- yak_error_handler/example/bin/example.dart
+++ yak_error_handler/example/bin/example.dart
@@ -10,10 +10,15 @@ void veryBadAPI() => _random.nextBool() ? {} : throw 'ops';
 
 void main() {
   final container = ProviderContainer();
-  final _runner = YakRunner(veryBadAPI, container.read(catchError));
+
+  /// !! `IMPORTANT`  set the catchError before everything else!
   container.read(setCatchError)(
       (e, s) => print('****** ERRROR COUGHT *******\n$e\n$s'));
+
+  final _runner = YakRunner(veryBadAPI, container.read(catchError));
+
   for (var i = 0; i < 10; ++i) {
+    print('$i');
     _runner().when(
       success: (_) {},
       failure: (_, __) {},
