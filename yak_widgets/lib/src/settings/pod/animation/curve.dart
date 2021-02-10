4ad5f6b854cb946400fd9ca04ea5a04d70f45eb9
Francesco Iapicca <iapicca@gmail.com>
2021-02-10 23:51:01 +0200

    yak_widgets fix pub.dev static analysis issue

--- yak_widgets/lib/src/settings/pod/animation/curve.dart
+++ yak_widgets/lib/src/settings/pod/animation/curve.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/all.dart';
 
-const _easeOut = Curves.easeOut;
-
 /// `StateProvider` for `Curve`
-final animationCurvePod = StateProvider<Curve>((ref) => _easeOut);
+final animationCurvePod = StateProvider<Curve>((ref) => Curves.easeOut);
