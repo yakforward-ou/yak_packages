fbe47b0dbf0b4fdf11f11eda1f03b653bb3d7841
Francesco Iapicca <iapicca@gmail.com>
2021-02-11 09:10:09 +0200

    yak_widgets updated dependencies

--- yak_widgets/lib/src/settings/pod/animation/curve.dart
+++ yak_widgets/lib/src/settings/pod/animation/curve.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:hooks_riverpod/all.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 /// `StateProvider` for `Curve`
 final animationCurvePod = StateProvider<Curve>((ref) => Curves.easeOut);
