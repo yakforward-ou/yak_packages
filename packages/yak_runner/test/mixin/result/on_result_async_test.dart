d58b85ea939b76e9da02e8d004827eb1c7857f2c
Francesco Iapicca <iapicca@gmail.com>
2021-02-12 15:46:57 +0200

    yak_runner reverting to test from flutter_test

--- packages/yak_runner/test/mixin/result/on_result_async_test.dart
+++ packages/yak_runner/test/mixin/result/on_result_async_test.dart
@@ -1,7 +1,7 @@
 import 'dart:async';
 
 import 'package:mockito/mockito.dart';
-import 'package:flutter_test/flutter_test.dart';
+import 'package:test/test.dart';
 import 'package:yak_runner/yak_runner.dart';
 import '../../mocks/all.dart';
 
