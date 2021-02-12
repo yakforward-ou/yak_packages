20d48161ebf89c3559b2d593c95e366865748a42
Francesco Iapicca <iapicca@gmail.com>
2021-02-12 15:07:34 +0200

    fixing fixable tests

--- packages/yak_math/test/tweens/curve_test.dart
+++ packages/yak_math/test/tweens/curve_test.dart
@@ -4,17 +4,28 @@ import 'package:yak_math/yak_math.dart';
 
 void main() {
   group('`YakCurveTween` test', () {
+    final value = Curves.bounceIn;
+    final yakTween = YakCurveTween(curve: value);
+
     test(
         'WHEN `YakCurveTween` and `CurveTween` transform '
         'THEN output is identical', () {
-      final value = Curves.bounceIn;
       const lerp = 1 / 3;
-      final yakTween = YakCurveTween(curve: value);
       final tween = CurveTween(curve: value);
       expect(
         yakTween.transform(lerp),
         tween.transform(lerp),
         reason: '`YakCurveTween` & `CurveTween` transform the same output',
+      );
+    });
+    test('`toString` works as intended', () {
+      final argumentName = 'curve: ';
+      final objectName = 'YakCurveTween';
+      final match = '$objectName($argumentName$value)';
+      expect(
+        '$yakTween',
+        match,
+        reason: '`toString` output must be predictable',
       );
     });
   });
--- packages/yak_math/test/tweens/text_style_test.dart
+++ packages/yak_math/test/tweens/text_style_test.dart
@@ -4,11 +4,11 @@ import 'package:yak_math/yak_math.dart';
 
 void main() {
   group('`YakTextStyleTween` test', () {
+    const begin = TextStyle(color: Color.fromRGBO(255, 0, 0, 1));
+    const end = TextStyle(color: Color.fromRGBO(255, 255, 0, 1));
     test(
         'WHEN `YakTextStyleTween` and `TextStyleTween` lerp '
         'THEN output is identical', () {
-      const begin = TextStyle(color: Color.fromRGBO(255, 0, 0, 1));
-      const end = TextStyle(color: Color.fromRGBO(255, 255, 0, 1));
       const lerp = 1 / 3;
       const yakTween = YakTextStyleTween(begin: begin, end: end);
       final tween = TextStyleTween(begin: begin, end: end);
