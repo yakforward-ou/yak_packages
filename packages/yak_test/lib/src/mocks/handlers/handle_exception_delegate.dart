d2a7de32e5af054101986c03822db32c8c66ac5a
Francesco Iapicca <iapicca@gmail.com>
2021-02-24 22:28:36 +0200

    fix typo

--- packages/yak_test/lib/src/mocks/handlers/handle_exception_delegate.dart
+++ packages/yak_test/lib/src/mocks/handlers/handle_exception_delegate.dart
@@ -1,6 +1,6 @@
 /// a delegate for `yak_runer` typedef `HandleException`
 
 mixin ExceptionHandler {
-  /// implements yak_runer`'s `HandleException`
+  /// implements `yak_runer`'s `HandleException`
   void call(Exception e, StackTrace s);
 }
