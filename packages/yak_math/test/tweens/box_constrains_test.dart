394db21bfce3c039a7900ad7bd8d5dc6cfc777eb
Francesco Iapicca <iapicca@gmail.com>
2021-03-11 11:51:37 +0200

    revert delete assertion

--- packages/yak_math/test/tweens/box_constrains_test.dart
+++ packages/yak_math/test/tweens/box_constrains_test.dart
@@ -29,5 +29,31 @@ void main() {
             'lerp the same output',
       );
     });
+
+    test(
+        'WHEN `YakBoxConstraintsTween` and `BoxConstraintsTween` lerp '
+        'THEN output is identical', () {
+      const begin = BoxConstraints(
+        maxHeight: 200,
+        maxWidth: 200,
+        minHeight: 0,
+        minWidth: 0,
+      );
+      const end = BoxConstraints(
+        maxHeight: 400,
+        maxWidth: 400,
+        minHeight: 200,
+        minWidth: 200,
+      );
+      const lerp = 1 / 3;
+      const yakTween = YakBoxConstraintsTween(begin: begin, end: end);
+      final tween = BoxConstraintsTween(begin: begin, end: end);
+      expect(
+        yakTween.lerp(lerp),
+        tween.lerp(lerp),
+        reason: '`YakBoxConstraintsTween` & `BoxConstraintsTween` '
+            'lerp the same output',
+      );
+    });
   });
 }
