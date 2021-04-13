b30c64b3886ed57d8172ee91869ab0db5c319754
iapicca <iapicca@gmail.com>
2021-04-13 16:23:43 +0300

    fails initial build with hook

--- packages/yak_widgets/example/index_selector/lib/main.dart
+++ packages/yak_widgets/example/index_selector/lib/main.dart
@@ -3,29 +3,31 @@ import 'package:flutter_hooks/flutter_hooks.dart';
 
 typedef OnIndexChanged = void Function(int);
 
-/// a widget that allows to select an action according to an index
-class IndexSelector extends HookWidget {
-  /// a list of widget
-  final IndexedWidgetBuilder builder;
+typedef FocusedIndexedItemBuilder = Widget Function(
+    BuildContext context, int index, bool focus);
 
-  /// the c
+/// a widget that allows to select an action according to an index
+class FocusedIndexList extends HookWidget {
+  /// a list of widget
+  final FocusedIndexedItemBuilder builder;
+
+  /// the number of item in the selector
   final int itemCount;
 
   /// a callbackinitialData
   final OnIndexChanged? onIndexChanged;
 
-  /// triggers on tap even if the index does not change
-  final bool alwaysTriggerOnTap;
-
   final int focusedIndex;
 
   final Duration? duration;
 
+  final AlignmentGeometry childrenAlignment;
+
   /// no point for a const constructor
-  IndexSelector({
+  FocusedIndexList({
     Key? key,
+    this.childrenAlignment = Alignment.center,
     this.onIndexChanged,
-    this.alwaysTriggerOnTap = false,
     this.focusedIndex = 0,
     this.duration,
     required this.builder,
@@ -39,26 +41,17 @@ class IndexSelector extends HookWidget {
   @override
   Widget build(BuildContext context) {
     final controller = useScrollController();
-    final currentIndex = useState(focusedIndex);
-    //final currentIndex = useState(0);
+    final focused = useState(focusedIndex);
 
     OnIndexChanged scrollFromHeight(double height) {
-      return (index) {
+      return (remainder) {
         controller.animateTo(
-          height * index, //+ focusedIndex,
+          height * remainder,
           duration: duration ?? kThemeAnimationDuration,
           curve: Curves.fastOutSlowIn,
         );
       };
     }
-
-    useMemoized(() {
-      if (onIndexChanged != null) {
-        currentIndex.addListener(() {
-          onIndexChanged!(currentIndex.value);
-        });
-      }
-    });
 
     return LayoutBuilder(
       key: ValueKey('LayoutBuilder@IndexedSelector:$key'),
@@ -66,50 +59,37 @@ class IndexSelector extends HookWidget {
         final height = constraints.maxHeight / itemCount;
         final scroll = scrollFromHeight(height);
 
-        GestureTapCallback onTap(index) {
-          return () {
-            currentIndex.value = index;
-            scroll(index);
-          };
-        }
-
         return ListView.builder(
           key: ValueKey('ListView.builder@IndexedSelector:$key'),
-          physics: NeverScrollableScrollPhysics(),
+          physics: const NeverScrollableScrollPhysics(),
           controller: controller,
           itemBuilder: (context, index) {
-            final rem = (index - focusedIndex).abs() % itemCount;
+            final remainder =
+                (index - focusedIndex + itemCount).abs() % itemCount;
+            final selected = focused.value == remainder;
             return SizedBox(
-              key: ValueKey('SizedBox:$index@IndexedSelector:$key'),
               height: height,
-              child: ValueListenableBuilder<int>(
-                key: ValueKey(
-                  'ValueListenableBuilder:$index@IndexedSelector:$key',
-                ),
-                valueListenable: currentIndex,
-                child: Center(
-                  key: GlobalKey(
-                    debugLabel: 'Center$index@IndexedSelector:$key',
-                  ),
-                  child: builder(context, rem),
-                ),
-                builder: (context, value, child) {
-                  return value != rem
-                      ? GestureDetector(
-                          key: ValueKey(
-                            'GestureDetector$index:@IndexedSelector:$key',
-                          ),
-                          onTap: onTap(rem),
-                          child: AbsorbPointer(
-                            key: ValueKey(
-                              'AbsorbPointer$index:$value@IndexedSelector:$key',
-                            ),
-                            absorbing: true,
-                            child: child,
-                          ),
-                        )
-                      : child!;
+              child: GestureDetector(
+                onTap: () {
+                  if (!selected) {
+                    onIndexChanged?.call(remainder);
+                    focused.value = remainder;
+                    scroll(remainder);
+                  }
                 },
+                child: ValueListenableBuilder(
+                  valueListenable: focused,
+                  builder: (context, value, child) {
+                    return Align(
+                      alignment: childrenAlignment,
+                      child: builder(
+                        context,
+                        remainder,
+                        selected,
+                      ),
+                    );
+                  },
+                ),
               ),
             );
           },
@@ -120,16 +100,65 @@ class IndexSelector extends HookWidget {
 }
 
 void main() {
-  runApp(MaterialApp(
-    home: IndexSelector(
-      duration: const Duration(milliseconds: 400),
-      onIndexChanged: (i) => print('index changed to $i'),
-      focusedIndex: 0,
-      itemCount: 5,
-      builder: (context, index) => TextButton(
-        onPressed: () => print('$index pressed'),
-        child: Text('$index'),
+  runApp(
+    MaterialApp(
+      home: Material(
+        child: FocusedIndexList(
+          duration: const Duration(milliseconds: 400),
+          onIndexChanged: (i) => print('index changed to $i'),
+          focusedIndex: 2,
+          itemCount: 5,
+          builder: (context, index, selected) {
+            final child = Text('$index');
+            return selected
+                ? TextButton(
+                    onPressed: () => print('$index pressed'),
+                    child: child,
+                  )
+                : child;
+          },
+        ),
       ),
     ),
-  ));
+  );
 }
+
+
+
+
+            // final center = Align(
+            //   alignment: childrenAlignment ?? Alignment.center,
+            //   key: ValueKey(
+            //     'Center$index@IndexedSelector:$key',
+            //   ),
+            //   child: builder(context, remainder, selected.value == rem ),
+            // );
+
+            // final detector = GestureDetector(
+            //   key: ValueKey(
+            //     'GestureDetector$remainder:@IndexedSelector:$key',
+            //   ),
+            //   onTap: onTap(remainder),
+            //   child: AbsorbPointer(
+            //     key: ValueKey(
+            //       'AbsorbPointer$remainder@IndexedSelector:$key',
+            //     ),
+            //     absorbing: true,
+            //     child: center,
+            //   ),
+            // );
+
+            // return SizedBox(
+            //   key: ValueKey('SizedBox:$index@IndexedSelector:$key'),
+            //   height: height,
+            //   child: ValueListenableBuilder<int>(
+            //     key: ValueKey(
+            //       'ValueListenableBuilder:$index@IndexedSelector:$key',
+            //     ),
+            //     valueListenable: focused,
+            //     builder: (context, value, child) {
+            //       print('index: $index, value: $value, rem: $rem');
+            //       return value != rem ? detector : center;
+            //     },
+            //   ),
+            // );
