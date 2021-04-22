4340574e80fd240e66731b9610bc037dae0da8c2
iapicca <iapicca@gmail.com>
2021-04-22 12:44:13 +0300

    quickly fixed yak_runner analisis

--- packages/yak_runner/lib/src/extensions/failure/of_type.dart
+++ packages/yak_runner/lib/src/extensions/failure/of_type.dart
@@ -1,7 +1,8 @@
 import '../../../yak_runner.dart';
+//  ignore_for_file: avoid_shadowing_type_parameters
 
 /// cast a `Failure<S>` to a `Failure<T`
 extension FailureToFailureOfTypeX<T, S> on Failure<S> {
   /// matches the typedef `FailureOfType<T>`
-  Failure<T> ofType<T>() => Failure<T>(e, s);
+  Failure<T> ofType<T>() => Failure(e, s);
 }
