383bfddbbad4f9df112405a958068ffbc387c971
iapicca <iapicca@gmail.com>
2021-04-22 14:50:06 +0300

    need to update yak_math first

---
+++ packages/yak_runner/lib/src/extensions/runner/copy_with.dart
@@ -0,0 +1,16 @@
+// import 'package:yak_error_handler/yak_error_handler.dart';
+
+// import '../../../yak_runner.dart';
+
+// ///
+
+// extension CopyWithX<T> on Runner<T> {
+//   ///
+//   Runner<T> copyWidth({
+//     HandleExceptionDelegate? exceptionHandler,
+//     Set<ErrorHandler> errorHandlers = const {},
+//     List<OnSuccessCallback<T>> onSuccess = const [],
+//   }) {
+
+//   }
+// }
