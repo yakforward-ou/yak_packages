3a356788e454cb4093f70774c3cb6b97c3f11e50
iapicca <iapicca@gmail.com>
2021-05-10 16:00:49 +0300

    only aesthetic changes

--- packages/yak_runner/lib/src/extensions/custom/json/decode.dart
+++ packages/yak_runner/lib/src/extensions/custom/json/decode.dart
@@ -3,17 +3,12 @@ import '../../../../yak_runner.dart';
 /// tries to decode a `String`
 extension MapSafelyDecodeX on String {
   /// returns a `Result<Map<String, Object?>>`
-  Result<Map<String, Object?>> jsonDecodeAsMap() {
-    final runner = JsonDecodeToMap();
-    return runner(this);
-  }
+  Result<Map<String, Object?>> jsonDecodeAsMap() => JsonDecodeToMap()(this);
 }
 
 /// tries to decode a `String`
 extension ListSafelyDecodeX on String {
   /// returns a `Result<List<Map<String, Object?>>`
-  Result<List<Map<String, Object?>>> jsonDecodeAsList() {
-    final runner = JsonDecodeToList();
-    return runner(this);
-  }
+  Result<List<Map<String, Object?>>> jsonDecodeAsList() =>
+      JsonDecodeToList()(this);
 }
--- packages/yak_runner/lib/src/extensions/custom/json/encode.dart
+++ packages/yak_runner/lib/src/extensions/custom/json/encode.dart
@@ -3,17 +3,11 @@ import '../../../../yak_runner.dart';
 /// tries to encode to json a ` Map<String, Object?>`
 extension MapSafelyEncodeX on Map<String, Object?> {
   /// returns a `Result<String>`
-  Result<String> encode() {
-    final runner = JsonEncodeFromMap();
-    return runner(this);
-  }
+  Result<String> encode() => JsonEncodeFromMap()(this);
 }
 
 /// tries to encode to json a `List<Map<String, Object?>>`
 extension ListSafelyEncodeX on List<Map<String, Object?>> {
   /// returns a `Result<String>`
-  Result<String> encode() {
-    final runner = JsonEncodeFromList();
-    return runner(this);
-  }
+  Result<String> encode() => JsonEncodeFromList()(this);
 }
