ccbc5275c8f95abc80763f1230504e926972ae18
iapicca <iapicca@gmail.com>
2021-05-12 12:40:18 +0300

    fixed runner analysis

--- packages/yak_runner/lib/src/extensions/custom/json/parse_json.dart
+++ packages/yak_runner/lib/src/extensions/custom/json/parse_json.dart
@@ -3,7 +3,7 @@ import '../../../../yak_runner.dart';
 /// an extension that returns a `Result<T>` from a `json`
 extension ParseAsJsonX<T> on Map<String, Object?> {
   /// returns a `Result<T>`
-  Result<T> parseAsJson<T>(ParseJson<T> parse) {
+  Result<T> parseAsJson(ParseJson<T> parse) {
     final runner = JsonParser(parse);
     return runner(this);
   }
