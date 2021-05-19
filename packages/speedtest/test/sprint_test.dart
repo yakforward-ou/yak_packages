020e401d7ba93c6cd3ebf0e45cf8848d0a2a444e
iapicca <iapicca@gmail.com>
2021-05-19 14:36:25 +0300

    Sprint test added

--- packages/speedtest/test/sprint_test.dart
+++ packages/speedtest/test/sprint_test.dart
@@ -1,21 +1,56 @@
-import 'dart:math';
-
 import 'package:speedtest/src/sprint.dart';
+import 'package:stub/stub.dart';
 import 'package:test/test.dart';
+// import 'package:yak_test/yak_test.dart';
 
 void main() {
-  const d = Duration(milliseconds: 100);
-  const parameter = 100;
-  void subject() async => await Future.delayed(d);
+  const d = Duration(milliseconds: 10);
+  const parameter = 10;
+  final subject = nullaryStub<void>()
+    ..stub = () async => await Future.delayed(d);
+  final benchmark = SprintTest(subject: subject.wrap, parameter: parameter);
 
   group('SprintTest', () {
     test(
         'GIVEN a `subject` function of duration `d` '
         'WHEN `SprintTest` parameter `n` '
         'THEN call return ~d*n', () {
-      final benchmark = SprintTest(subject: subject, parameter: parameter);
+      subject.reset;
 
-      expect(benchmark()!.inMilliseconds/parameter, moreOrLessEquals(d.inMilliseconds));
+      expect(
+        benchmark(),
+        isNot(isNull),
+        reason: 'should be true',
+      );
+    });
+
+    /// add better test after ammending yak_test
+    test(
+        'GIVEN a `subject` throws `Exception` '
+        'WHEN `SprintTest` '
+        'THEN call return null', () {
+      subject.reset;
+      subject.stub = () => throw Exception();
+
+      expect(
+        benchmark(),
+        isNull,
+        reason: 'should be null',
+      );
+    });
+
+    test(
+        'GIVEN a `subject` throws `Error` '
+        'WHEN `SprintTest` '
+        'THEN call return null', () {
+      subject.reset;
+      subject.stub = () => throw Error();
+
+      expect(
+        benchmark(),
+        isNull,
+        reason: 'should be null',
+      );
     });
   });
 }
