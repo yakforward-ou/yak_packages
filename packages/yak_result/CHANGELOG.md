da6a1d6220bbc1c445989f82350030cd1bbd37f5
iapicca <iapicca@gmail.com>
2022-07-15 16:51:11 +0300

    fix delegates typedefs

--- packages/yak_result/CHANGELOG.md
+++ packages/yak_result/CHANGELOG.md
@@ -1,4 +1,4 @@
-### 1.0.0+2
+### 1.0.0+3
 - major release
 - update typedefs
 - downgrade `meta` for `flutter` compatibility
--- packages/yak_result/lib/src/typedef/delegates.dart
+++ packages/yak_result/lib/src/typedef/delegates.dart
@@ -1,4 +1,9 @@
-import 'package:yak_utils/yak_utils.dart';
+import 'package:yak_utils/yak_utils.dart'
+    show
+        NullaryDelegate,
+        UnaryDelegate,
+        NullaryDelegateAsync,
+        UnaryDelegateAsync;
 
 import '../all.dart';
 
@@ -9,7 +14,7 @@ typedef ResultNullaryDelegate<T> = NullaryDelegate<Result<T>>;
 typedef ResultUnaryDelegate<T, S> = UnaryDelegate<Result<T>, S>;
 
 /// a typedef for an [async] [NullaryDelegate] of [Result]
-typedef ResultNullaryDelegateAsync<T> = ResultNullaryDelegate<Future<T>>;
+typedef ResultNullaryDelegateAsync<T> = NullaryDelegateAsync<Result<T>>;
 
 /// a typedef for an [async] [UnaryDelegate] of [Result]
-typedef ResultUnaryDelegateAsync<T, S> = ResultUnaryDelegate<Future<T>, S>;
+typedef ResultUnaryDelegateAsync<T, S> = UnaryDelegateAsync<Result<T>, S>;
