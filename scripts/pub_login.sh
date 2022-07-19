8d36095d544afc334c1cb2f2a80087561f6052b2
iapicca <iapicca@gmail.com>
2022-07-19 11:45:46 +0300

    remove endpoint refs from publish workflow and script

--- scripts/pub_login.sh
+++ scripts/pub_login.sh
@@ -13,11 +13,6 @@ if [ -z "${PUB_DEV_PUBLISH_REFRESH_TOKEN}" ]; then
   exit 1
 fi
 
-if [ -z "${PUB_DEV_PUBLISH_TOKEN_ENDPOINT}" ]; then
-  echo "Missing PUB_DEV_PUBLISH_TOKEN_ENDPOINT environment variable"
-  exit 1
-fi
-
 if [ -z "${PUB_DEV_PUBLISH_EXPIRATION}" ]; then
   echo "Missing PUB_DEV_PUBLISH_EXPIRATION environment variable"
   exit 1
