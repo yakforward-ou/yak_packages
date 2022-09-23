09be1c01db96fa263b5c126c23a8443d7aab2b08
iapicca <iapicca@gmail.com>
2022-09-23 09:29:53 +0300

    [yak_result] update s

--- packages/yak_result/lib/src/classes/result.dart
+++ packages/yak_result/lib/src/classes/result.dart
@@ -1,14 +1,26 @@
+import 'package:meta/meta.dart';
+import 'package:yak_utils/yak_utils.dart';
+import 'all.dart';
+
 /// represent the outcome of a functionf
 abstract class Result<T> {
   /// allows a const constructor
   const Result();
 
+  const factory Result.success(T data) = Success;
+  const factory Result.pending() = Pending;
+  factory Result.failure({Object? reason, StackTrace? stackTrace}) = Failure;
+
   @override
   bool operator ==(Object other) => hashCode == other.hashCode;
 
   @override
-  int get hashCode => runtimeType.hashCode;
+  int get hashCode => Object.hash(runtimeType, T.runtimeType);
 
   @override
-  String toString() => '$runtimeType';
+  @nonVirtual
+  String toString() => '${toJson()}';
+
+  /// allow compatibility with [dart:convert]
+  Json toJson();
 }
