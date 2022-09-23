b0d223844c60c7586f67aea6e954fc4b98198ab8
iapicca <iapicca@gmail.com>
2022-09-23 09:46:54 +0300

    [yak_utils] remove unnecessary comments

--- packages/yak_utils/lib/src/extensions/then_sync.dart
+++ packages/yak_utils/lib/src/extensions/then_sync.dart
@@ -8,13 +8,3 @@ import '../types/all.dart';
 extension ThenSyncX<T, S> on S {
   T then<R extends Unary<T, S>>(R function) => function(this);
 }
-
-// /// an extension that allow to concatenate async functions
-// ///
-// /// ```dart
-// /// final result = await begin.let(foo).let(bar).let(baz);
-// /// ```
-// extension LetAsyncX<T, S> on FutureOr<S> {
-//   Future<T> letAsync<R extends UnaryAsync<T, S>>(R function) async =>
-//       await function(await this);
-// }
