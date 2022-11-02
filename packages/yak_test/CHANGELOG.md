d3f1577457bcdf41a31f580f5dd8a102b1a67070
Francesco Iapicca <iapicca@gmail.com>
2022-11-02 18:27:22 +0200

    [yak_test] update changelog

--- packages/yak_test/CHANGELOG.md
+++ packages/yak_test/CHANGELOG.md
@@ -1,4 +1,4 @@
-###  1.0.1+0
+###  1.0.1+1
 - add `constructorTester`
 - update `delegate_tester`
 - update `equality tests`
--- packages/yak_utils/test/tools/union_test.dart
+++ packages/yak_utils/test/tools/union_test.dart
@@ -3,10 +3,11 @@ import 'package:yak_test/yak_test.dart';
 import 'package:yak_utils/yak_utils.dart';
 
 void main() {
-  group('Union', () {
-    equalityTest(
+  group(
+    'Union',
+    () => equalityTest(
       () => Union('hello', 42),
       () => Union('world', 0),
-    );
-  });
+    ),
+  );
 }
