a09e556e4dd5869c1afe5da3f1e8ffd87a2a2874
Francesco Iapicca <iapicca@gmail.com>
2022-12-30 12:55:54 +0200

    [yak_runner] implement and test letRun and thenRun

--- packages/yak_runner/lib/src/extension/all.dart
+++ packages/yak_runner/lib/src/extension/all.dart
@@ -3,3 +3,4 @@ export 'run.dart';
 export 'run_delegate.dart';
 export 'run_callback.dart';
 export 'run_callback_delegate.dart';
+export 'then_run.dart';
---
+++ packages/yak_runner/lib/src/extension/then_run.dart
@@ -0,0 +1,32 @@
+import 'dart:async';
+
+import 'package:yak_result/yak_result.dart';
+import 'package:yak_utils/yak_utils.dart';
+import '../all.dart';
+
+/// syntactic sugar to pass a [ValueResult] to a [ResultUnary]
+extension ValueResultLetRunX<S> on ValueResult<S> {
+  ValueResult<T> letRun<T>(Unary<T, S> function) =>
+      isSuccess ? function.run(value) : failure.recast<T>();
+}
+
+/// syntactic sugar to pass a [FutureValueResult] to a [ResultUnaryAsync]
+extension ValueResultThenRunX<S> on FutureValueResult<S> {
+  FutureValueResult<T> thenRun<T>(UnaryFutureOr<T, S> function) =>
+      Future.sync(() => this).then((result) => result.isSuccess
+          ? function.runAsync(result.value)
+          : result.failure.recast());
+}
+
+/// syntactic sugar to pass a [VoidResult] to a [ResultNullary]
+extension VoidResultLetRunX on VoidResult {
+  ValueResult<T> letRun<T>(Nullary<T> function) =>
+      isSuccess ? function.run() : failure.recast<T>();
+}
+
+/// syntactic sugar to pass a [FutureVoidResult] to a [ResultNullaryAsync]
+extension VoidResultThenRunX<T> on FutureVoidResult {
+  FutureValueResult<T> thenRun(NullaryFutureOr<T> function) =>
+      Future.sync(() => this).then((result) =>
+          result.isSuccess ? function.runAsync() : result.failure.recast());
+}
