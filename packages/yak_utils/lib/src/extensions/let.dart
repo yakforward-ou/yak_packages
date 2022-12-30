ba837ee75dbfbfbf5a717bcedfdc8fecdbaf1601
Francesco Iapicca <iapicca@gmail.com>
2022-12-30 15:19:36 +0200

    [yak_utils] bugfix for LetX

--- packages/yak_utils/lib/src/extensions/let.dart
+++ packages/yak_utils/lib/src/extensions/let.dart
@@ -1,6 +1,6 @@
 import '../types/all.dart';
 
 /// an extension on an object that uses it in a given unary
-extension LetX<T, S> on T {
-  S let(Unary<S, T> function) => function(this);
+extension LetX<T> on T {
+  S let<S>(Unary<S, T> function) => function(this);
 }
