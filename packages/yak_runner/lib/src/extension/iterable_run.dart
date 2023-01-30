c8484e71990f8e2f2eff344116205322a7491658
Francesco Iapicca <iapicca@gmail.com>
2023-01-30 16:59:04 +0200

    [yak_runner] add missing tests for iterable_run

--- packages/yak_runner/lib/src/extension/iterable_run.dart
+++ packages/yak_runner/lib/src/extension/iterable_run.dart
@@ -34,3 +34,37 @@ extension IterableUnaryToResultUnaryX<T, S> on Iterable<Unary<T, S>> {
     }
   }
 }
+
+extension IterableNullaryToResultNullaryAsyncX<T>
+    on Iterable<NullaryFutureOr<T>> {
+  /// returns  Iterable<ResultNullary<T>> from Iterable<Nullary<T>>
+  Iterable<ResultNullaryAsync<T>> get runAsync sync* {
+    for (final function in this) {
+      yield function.runAsync;
+    }
+  }
+
+  /// returns Iterable<VoidResultNullary> from Iterable<Nullary<T>>
+  Iterable<VoidResultNullaryAsync> get runVoidAsync sync* {
+    for (final function in this) {
+      yield function.runVoidAsync;
+    }
+  }
+}
+
+extension IterableUnaryToResultUnaryAsyncX<T, S>
+    on Iterable<UnaryFutureOr<T, S>> {
+  /// returns  Iterable<ResultNullary<T>> from Iterable<Unary<T, S>>
+  Iterable<ResultUnaryAsync<T, S>> get runAsync sync* {
+    for (final function in this) {
+      yield function.runAsync;
+    }
+  }
+
+  /// returns  Iterable<VoidResultUnary<S>> from Iterable<Unary<T, S>>
+  Iterable<VoidResultUnaryAsync<S>> get runVoidAsync sync* {
+    for (final function in this) {
+      yield function.runVoidAsync;
+    }
+  }
+}
---
+++ packages/yak_runner/test/extensions/iterable_run_test.dart
@@ -0,0 +1,142 @@
+import 'package:stub/stub.dart';
+import 'package:test/test.dart';
+import 'package:yak_runner/yak_runner.dart';
+
+void main() {
+  group('IterableNullaryToResultNullaryX', () {
+    group('run', () {
+      final tester = Stub.nullary<List<Nullary<int>>>()..stub = () => [];
+
+      setUp(tester.reset);
+
+      test(
+          'GIVEN a tester List<Nullary<int> '
+          'WHEN tester().run '
+          'THEN returns a Iterable<ResultNullary<int>>', () {
+        expect(
+          tester().run,
+          isA<Iterable<ResultNullary<int>>>(),
+          reason: 'Type should be predictable',
+        );
+      });
+    });
+    group('runVoid', () {
+      final tester = Stub.nullary<List<Nullary<int>>>()..stub = () => [];
+
+      setUp(tester.reset);
+      test(
+          'GIVEN a tester List<Nullary<int> '
+          'WHEN tester().runVoid '
+          'THEN returns a Iterable<VoidResultNullary>', () {
+        expect(
+          tester().runVoid,
+          isA<Iterable<VoidResultNullary>>(),
+          reason: 'Type should be predictable',
+        );
+      });
+    });
+  });
+
+  group('IterableUnaryToResultUnaryX', () {
+    group('run', () {
+      final tester = Stub.nullary<List<Unary<int, int>>>()..stub = () => [];
+
+      setUp(tester.reset);
+
+      test(
+          'GIVEN a tester List<Nullary<T> '
+          'WHEN tester().run '
+          'THEN returns a Iterable<ResultUnary<T, S>>', () {
+        expect(
+          tester().run,
+          isA<Iterable<ResultUnary<int, int>>>(),
+          reason: 'Type should be predictable',
+        );
+      });
+    });
+    group('runVoid', () {
+      final tester = Stub.nullary<List<Unary<int, int>>>()..stub = () => [];
+
+      setUp(tester.reset);
+      test(
+          'GIVEN a tester List<Nullary<int> '
+          'WHEN tester().runVoid '
+          'THEN returns a Iterable<VoidResultUnary<S>>', () {
+        expect(
+          tester().runVoid,
+          isA<Iterable<VoidResultUnary<int>>>(),
+          reason: 'Type should be predictable',
+        );
+      });
+    });
+  });
+
+  group('IterableNullaryToResultNullaryAsyncX', () {
+    group('run', () {
+      final tester = Stub.nullary<List<NullaryAsync<int>>>()..stub = () => [];
+
+      setUp(tester.reset);
+
+      test(
+          'GIVEN a tester Iterable<NullaryAsync<T> '
+          'WHEN tester().runAsync '
+          'THEN returns a  Iterable<ResultNullaryAsync<T>>', () {
+        expect(
+          tester().runAsync,
+          isA<Iterable<ResultNullaryAsync<int>>>(),
+          reason: 'Type should be predictable',
+        );
+      });
+    });
+    group('runVoid', () {
+      final tester = Stub.nullary<List<NullaryAsync<int>>>()..stub = () => [];
+
+      setUp(tester.reset);
+      test(
+          'GIVEN a tester List<NullaryAsync<int>> '
+          'WHEN tester().runVoidAsync '
+          'THEN returns a Iterable<VoidResultNullaryAsync>', () {
+        expect(
+          tester().runVoidAsync,
+          isA<Iterable<VoidResultNullaryAsync>>(),
+          reason: 'Type should be predictable',
+        );
+      });
+    });
+  });
+
+  group('group IterableUnaryToResultUnaryX', () {
+    group('run', () {
+      final tester = Stub.nullary<List<UnaryFutureOr<int, int>>>()
+        ..stub = () => [];
+
+      setUp(tester.reset);
+
+      test(
+          'GIVEN a tester Iterable<UnaryFutureOr<T, S>> '
+          'WHEN tester().runAsync '
+          'THEN returns a Iterable<ResultUnaryAsync<T, S>>', () {
+        expect(
+          tester().runAsync,
+          isA<Iterable<ResultUnaryAsync<int, int>>>(),
+          reason: 'Type should be predictable',
+        );
+      });
+    });
+    group('run', () {
+      final tester = Stub.nullary<List<Unary<int, int>>>()..stub = () => [];
+
+      setUp(tester.reset);
+      test(
+          'GIVEN a tester List<Nullary<int> '
+          'WHEN tester().runVoid '
+          'THEN returns aIterable<VoidResultUnaryAsync<S>>', () {
+        expect(
+          tester().runVoidAsync,
+          isA<Iterable<VoidResultUnaryAsync<int>>>(),
+          reason: 'Type should be predictable',
+        );
+      });
+    });
+  });
+}
