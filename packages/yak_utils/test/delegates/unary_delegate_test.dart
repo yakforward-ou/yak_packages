1b3708e9a82bbbc904d97542c0907f43c2bd38b2
Francesco Iapicca <iapicca@gmail.com>
2023-05-16 12:44:19 +0000

    fix https://github.com/iapicca/yak_packages/issues/223

--- packages/yak_utils/test/delegates/unary_delegate_test.dart
+++ packages/yak_utils/test/delegates/unary_delegate_test.dart
@@ -1,31 +1,26 @@
+import 'package:test/test.dart';
+import 'package:yak_test/yak_test.dart';
+import 'package:yak_utils/yak_utils.dart';
+import 'package:mocktail/mocktail.dart';
+
+class _UnaryTester extends Mock implements UnaryDelegate<int, bool> {}
+
+class _UnaryTesterAsync extends Mock implements UnaryDelegateAsync<int, bool> {}
+
 void main() {
-  /// TODO re enable when [stub] and [yak_test] are re-published
+  group(
+    'UnaryDelegate Test',
+    () {
+      constructorTester(_UnaryTester.new);
+      unaryDelegateTest(_UnaryTester.new);
+    },
+  );
+
+  group(
+    'UnaryDelegateAsync Test',
+    () {
+      constructorTester(_UnaryTesterAsync.new);
+      unaryDelegatAsyncTest(_UnaryTesterAsync.new);
+    },
+  );
 }
-
-
-// import 'package:test/test.dart';
-// import 'package:yak_test/yak_test.dart';
-// import 'package:yak_utils/yak_utils.dart';
-// import 'package:mocktail/mocktail.dart';
-
-// class _UnaryTester extends Mock implements UnaryDelegate<int, bool> {}
-
-// class _UnaryTesterAsync extends Mock implements UnaryDelegateAsync<int, bool> {}
-
-// void main() {
-//   group(
-//     'UnaryDelegate Test',
-//     () {
-//       constructorTester(_UnaryTester.new);
-//       unaryDelegateTest(_UnaryTester.new);
-//     },
-//   );
-
-//   group(
-//     'UnaryDelegateAsync Test',
-//     () {
-//       constructorTester(_UnaryTesterAsync.new);
-//       unaryDelegatAsyncTest(_UnaryTesterAsync.new);
-//     },
-//   );
-// }
--- packages/yak_utils/test/tools/cache_test.dart
+++ packages/yak_utils/test/tools/cache_test.dart
@@ -1,106 +1,102 @@
+import 'dart:async';
+
+import 'package:stub/stub.dart';
+import 'package:test/test.dart';
+import 'package:yak_utils/yak_utils.dart';
+
 void main() {
-  /// TODO re enable when [stub] and [yak_test] are re-published
+  group('Cache', () {
+    const value = 1;
+
+    group('async', () {
+      late Cache<int> tester;
+      final generator = Stub.nullary<FutureOr<int>>()
+        ..stub = () => Future.sync(() => 1);
+
+      setUp(() {
+        tester = Cache<int>(generator);
+        generator.reset();
+      });
+
+      test(
+          'GIVEN a Cache<T> '
+          'WHEN is not initialized '
+          'THEN ready returns false', () {
+        expect(
+          tester.ready,
+          isFalse,
+          reason: 'cache should not be ready',
+        );
+        expect(
+          generator.count,
+          isZero,
+          reason: 'generator should not be called',
+        );
+      });
+      test(
+          'GIVEN a Cache<T> '
+          'WHEN is not initialized '
+          'THEN generator is not called', () {
+        expect(
+          generator.count,
+          isZero,
+          reason: 'generator should not be called',
+        );
+      });
+      test(
+          'GIVEN a Cache<T> '
+          'WHEN is initialized '
+          'THEN ready returns true', () async {
+        await tester.initialize();
+        expect(
+          tester.ready,
+          isTrue,
+          reason: 'Cache should  be ready',
+        );
+      });
+      test(
+          'GIVEN a Cache<T> '
+          'WHEN is initialized '
+          'THEN generator is called', () async {
+        await tester.initialize();
+        expect(
+          generator.count,
+          equals(1),
+          reason: 'generator should be called once',
+        );
+      });
+      test(
+          'GIVEN a Cache<T> '
+          'WHEN is initialized '
+          'THEN "value" returns expected value', () async {
+        await tester.initialize();
+        expect(
+          tester.value,
+          equals(value),
+          reason: 'value should be predictable',
+        );
+      });
+      test(
+          'GIVEN a Cache<T> '
+          'WHEN is not initialized '
+          'THEN "value" should throw', () {
+        expect(
+          () => tester.value,
+          throwsA(isA<Error>()),
+          reason: 'value should throw',
+        );
+      });
+      test(
+          'GIVEN a Cache<T> '
+          'WHEN generator throws '
+          'THEN initialization throws', () {
+        generator.stub = () => throw Exception();
+        expect(
+          () async => await tester.initialize(),
+          throwsException,
+          reason: 'value should throw',
+        );
+      });
+    });
+  });
 }
-
-// import 'dart:async';
-
-// import 'package:stub/stub.dart';
-// import 'package:test/test.dart';
-// import 'package:yak_utils/yak_utils.dart';
-
-// void main() {
-//   group('Cache', () {
-//     const value = 1;
-
-//     group('async', () {
-//       late Cache<int> tester;
-//       final generator = Stub.nullary<FutureOr<int>>()
-//         ..stub = () => Future.sync(() => 1);
-
-//       setUp(() {
-//         tester = Cache<int>(generator);
-//         generator.reset();
-//       });
-
-//       test(
-//           'GIVEN a Cache<T> '
-//           'WHEN is not initialized '
-//           'THEN ready returns false', () {
-//         expect(
-//           tester.ready,
-//           isFalse,
-//           reason: 'cache should not be ready',
-//         );
-//         expect(
-//           generator.count,
-//           isZero,
-//           reason: 'generator should not be called',
-//         );
-//       });
-//       test(
-//           'GIVEN a Cache<T> '
-//           'WHEN is not initialized '
-//           'THEN generator is not called', () {
-//         expect(
-//           generator.count,
-//           isZero,
-//           reason: 'generator should not be called',
-//         );
-//       });
-//       test(
-//           'GIVEN a Cache<T> '
-//           'WHEN is initialized '
-//           'THEN ready returns true', () async {
-//         await tester.initialize();
-//         expect(
-//           tester.ready,
-//           isTrue,
-//           reason: 'Cache should  be ready',
-//         );
-//       });
-//       test(
-//           'GIVEN a Cache<T> '
-//           'WHEN is initialized '
-//           'THEN generator is called', () async {
-//         await tester.initialize();
-//         expect(
-//           generator.count,
-//           equals(1),
-//           reason: 'generator should be called once',
-//         );
-//       });
-//       test(
-//           'GIVEN a Cache<T> '
-//           'WHEN is initialized '
-//           'THEN "value" returns expected value', () async {
-//         await tester.initialize();
-//         expect(
-//           tester.value,
-//           equals(value),
-//           reason: 'value should be predictable',
-//         );
-//       });
-//       test(
-//           'GIVEN a Cache<T> '
-//           'WHEN is not initialized '
-//           'THEN "value" should throw', () {
-//         expect(
-//           () => tester.value,
-//           throwsA(isA<Error>()),
-//           reason: 'value should throw',
-//         );
-//       });
-//       test(
-//           'GIVEN a Cache<T> '
-//           'WHEN generator throws '
-//           'THEN initialization throws', () {
-//         generator.stub = () => throw Exception();
-//         expect(
-//           () async => await tester.initialize(),
-//           throwsException,
-//           reason: 'value should throw',
-//         );
-//       });
-//     });
-//   });
-// }
