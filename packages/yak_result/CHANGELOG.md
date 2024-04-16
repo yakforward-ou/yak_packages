cbed58cd31378e253cf959db88bc3231889eb654
Francesco Iapicca <iapicca@email.com>
2024-04-16 11:13:12 +0300

    remove delegates from yak_result

--- packages/yak_result/CHANGELOG.md
+++ packages/yak_result/CHANGELOG.md
@@ -1,3 +1,6 @@
+### 2.1.0
+- fix yakforward-ou/yak_packages/issues/234
+
 ### 2.0.3
 - example link and readme update
 
--- packages/yak_result/lib/src/typedef/all.dart
+++ packages/yak_result/lib/src/typedef/all.dart
@@ -1,4 +1,3 @@
 export 'nullary_function.dart';
 export 'unary_function.dart';
-export 'delegates.dart';
 export 'future_result.dart';
