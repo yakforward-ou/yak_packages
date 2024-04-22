60b2e187f6ba40f3c02b25542eeeb6b042f1f6a1
Francesco Iapicca <iapicca@email.com>
2024-04-22 17:29:55 +0300

    [yak_flutter] add missing exports

--- packages/yak_flutter/lib/src/widgets/all.dart
+++ packages/yak_flutter/lib/src/widgets/all.dart
@@ -1,2 +1,5 @@
-export 'preferred_size_themed.dart';
 export 'edge_positioned.dart';
+export 'media_query_widget.dart';
+export 'preferred_size_themed.dart';
+export 'scalable_edge_positioned.dart';
+export 'scalable_widget.dart';
