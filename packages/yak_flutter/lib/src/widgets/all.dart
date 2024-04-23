59b6b50ee57b316e9c75d7797201d4382a7d82df
Francesco Iapicca <iapicca@email.com>
2024-04-23 17:46:33 +0300

    [yak_flutter] add inherited_value_notifier

--- packages/yak_flutter/lib/src/widgets/all.dart
+++ packages/yak_flutter/lib/src/widgets/all.dart
@@ -3,3 +3,4 @@ export 'media_query_widget.dart';
 export 'preferred_size_themed.dart';
 export 'scalable_edge_positioned.dart';
 export 'scalable_widget.dart';
+export 'inherited_value_notifier.dart';
