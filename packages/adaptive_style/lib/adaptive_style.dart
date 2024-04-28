75d55083acd33f84932e3fb2193a15b8d7c96e4b
Francesco Iapicca <iapicca@email.com>
2024-04-28 21:31:01 +0300

    [adaptive_style] remove unused data

--- packages/adaptive_style/lib/adaptive_style.dart
+++ packages/adaptive_style/lib/adaptive_style.dart
@@ -2,7 +2,6 @@ library adaptive_style;
 
 export 'src/adaptive_size_builder.dart';
 export 'src/adaptive_size_provider.dart';
-export 'src/_adaptive_size.dart';
 export 'src/device_size.dart';
 export 'src/extension.dart';
 export 'src/inherited_adaptive_size.dart';
